522eab2407bbdb5943083dff06bd2288b280b9c4
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2022-04-21 21:50:04 -0700

    ep9: typeorm + mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomersModule } from './customers/customers.module';
+import { User } from './entities/User';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CustomersModule, UsersModule],
+  imports: [
+    CustomersModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'username',
+      password: 'password',
+      database: 'nestjs_tutorial',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
---
+++ src/entities/User.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn({
+    type: 'bigint',
+    name: 'user_id',
+  })
+  public id: number;
+
+  @Column({ name: 'email_address', nullable: true, default: '' })
+  public emailAddress: string;
+
+  @Column({ nullable: false, default: '' })
+  public username: string;
+
+  @Column({ nullable: false, default: '' })
+  public password: string;
+}
